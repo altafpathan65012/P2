7f22b22b95b811759b8582f08aabade251646cd5
PATHAN <137278745+altafpathan65012@users.noreply.github.com>
2024-10-04 23:07:11 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "Jsjrjfuizjnsnbot"
-BOT_TOKEN = "6749533828:AAGUsdTyHDaeA24xCRa32Z5eH82sUWzhLjs"
+BOT_NAME = "pathansirtxttovideobot"
+BOT_TOKEN = "7790977411:AAEYgZS5xvHRJqH3YMPzv6twCQM3_MToiu4"
 API_ID = 23031620
 API_HASH = "31cb00c1cbe580394778b43105864bca"
 MONGO_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
