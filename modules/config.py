0e3bee0108e66aa04ce584fb3d29a9272855e4f6
PATHAN <137278745+altafpathan65012@users.noreply.github.com>
2024-10-07 12:31:21 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "pathansirtxttovideobot"
-BOT_TOKEN = "7790977411:AAG5lB1jr4Yt60rLyRHl00E0mlzJhwyzy8M"
+BOT_NAME = "DRMTXTBOT"
+BOT_TOKEN = "7512099467:AAHDki4rb2rwqfG0a5t6Ao1M8RWi8281L7E"
 API_ID = 23031620
 API_HASH = "31cb00c1cbe580394778b43105864bca"
 MONGO_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
