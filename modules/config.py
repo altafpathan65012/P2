38c586fe29dca2c7b6533d68c72992574cee30a5
PATHAN <137278745+altafpathan65012@users.noreply.github.com>
2024-10-17 12:41:51 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "SnndjdjdjjBot"
-BOT_TOKEN = "7852252079:AAHAxttDlyNpgNe4yBZylhi0t-s-Ef2bYTQ"
+BOT_NAME = "gfggdjsjsjdbot"
+BOT_TOKEN = "7774230851:AAFikhBJizAcW9IjjfS8mCZXlzX1-LO-eDA"
 API_ID = 23031620
 API_HASH = "31cb00c1cbe580394778b43105864bca"
 MONGO_URI = "mongodb+srv://pathanupsc:SuXqSnNocq3HnhXZ@cluster0.bpopu.mongodb.net/?retryWrites=true&w=majority"
