b22a2d2e80a3e1d80ff37b2765c9f5a4ab3d660d
PATHAN <137278745+altafpathan65012@users.noreply.github.com>
2024-10-18 23:11:08 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "gfggdjsjsjdbot"
-BOT_TOKEN = "7774230851:AAFikhBJizAcW9IjjfS8mCZXlzX1-LO-eDA"
+BOT_NAME = "DRMTXT1BOT"
+BOT_TOKEN = "7644865590:AAHr1IcXG2eXSRlgUy-lhA_N4dxgVnrdpDU"
 API_ID = 23031620
 API_HASH = "31cb00c1cbe580394778b43105864bca"
 MONGO_URI = "mongodb+srv://pathanupsc:SuXqSnNocq3HnhXZ@cluster0.bpopu.mongodb.net/?retryWrites=true&w=majority"
