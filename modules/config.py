05c78e8a232192c231a16919b46b97b1f78f10c3
PATHAN <137278745+altafpathan65012@users.noreply.github.com>
2024-11-04 22:20:15 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "drmnewdrmbot"
-BOT_TOKEN = "7170339260:AAHLIpBS1nSrork6K83HdwagVy9sQPDPoIk"
+BOT_NAME = "Byuiivyvyuygbot"
+BOT_TOKEN = "8028805856:AAHPuQjfowIc-CRDuOTJPCIv44852ccStfw"
 API_ID = 23031620
 API_HASH = "31cb00c1cbe580394778b43105864bca"
 MONGO_URI = "mongodb+srv://pathanupsc:SuXqSnNocq3HnhXZ@cluster0.bpopu.mongodb.net/?retryWrites=true&w=majority"
