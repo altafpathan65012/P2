618d3203723fc09d334494f6dfcc8bc83183f5f5
PATHAN <137278745+altafpathan65012@users.noreply.github.com>
2024-11-04 16:53:59 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -1071,7 +1071,7 @@ async def process_links(bot, m, links, b_name, count, end_count, raw_text2, res,
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
 
             elif "https://appx-transcoded-videos-mcdn.akamai.net.in/videos/sadhyamdigital-data/" in url:
-                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/bhainskipathshala-data/", "")
+                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/sadhyamdigital-data/", "")
                 name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "@").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
@@ -1082,8 +1082,9 @@ async def process_links(bot, m, links, b_name, count, end_count, raw_text2, res,
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
               
-            elif "https://appx-transcoded-videos-mcdn.akamai.net.in/videos/bhainskipathshala-data/" in url:
-                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/sadhyamdigital-data/", "")
+            
+            elif "https://appx-transcoded-videos-mcdn.akamai.net.in/videos/parmaracademy-data/" in url:
+                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/parmaracademy-data/", "")
                 name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "@").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
